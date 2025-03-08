b40595e2092ea63784c33b20f6696cbe92a7b982
Alvarado007 <samuel.alvarado@correounivalle.edu.co>
2025-03-07 22:21:28 -0500

    porfin

---
+++ src/main/java/com/alvaradoid/idproject/IdprojectApplication.java
@@ -0,0 +1,13 @@
+package com.alvaradoid.idproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvaradoid/idproject/IdprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvaradoid.idproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
