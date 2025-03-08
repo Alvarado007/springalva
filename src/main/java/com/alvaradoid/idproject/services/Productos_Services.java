2252afc3cbd39058ae84e6a9a3d354ffed7ef750
Alvarado007 <samuel.alvarado@correounivalle.edu.co>
2025-03-07 23:37:22 -0500

    se trabaja con services

---
+++ src/main/java/com/alvaradoid/idproject/services/Productos_Services.java
@@ -0,0 +1,22 @@
+package com.alvaradoid.idproject.services;
+import com.alvaradoid.idproject.models.Productos;
+import com.alvaradoid.idproject.repositorios.Repo_Productos;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Productos_Services {
+    private Repo_Productos repositorio = new Repo_Productos();
+
+    public List<Productos> finAll() {
+        return repositorio.finAll().stream().map(p->{
+            Double precioTotal=p.getPrecio()*1.50d;
+            p.setPrecio(precioTotal.intValue());
+            return p;
+        }).collect(Collectors.toList());
+    }
+    public Productos buscaId(long idproducto){
+        return repositorio.buscaId(idproducto);
+    }
+}
