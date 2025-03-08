e3fd5c8e5f7654a4d6e5f0304fa110ca04a11379
Alvarado007 <samuel.alvarado@correounivalle.edu.co>
2025-03-07 22:57:10 -0500

    se profundiza en la practica profesional de spring boot y como tabrja con las inyecciones de dependencia se hace modelo de productos donde se guarda una lista con productos dentro del constructor y se crea un metodo que puede retornar la lista y otro para buscar productos por id haciendo una comparacion

---
+++ src/main/java/com/alvaradoid/idproject/repositorios/Repo_Productos.java
@@ -0,0 +1,30 @@
+package com.alvaradoid.idproject.repositorios;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.alvaradoid.idproject.models.Productos;
+
+public class Repo_Productos {
+    List<Productos> datos;
+
+    public Repo_Productos() {
+        this.datos = Arrays.asList(
+            new Productos(1, "Producto 1", 100),
+            new Productos(2, "Producto 2", 200),
+            new Productos(3, "Producto 3", 300),
+            new Productos(4, "Producto 4", 400)
+        );
+    }
+    public List<Productos> finAll() {
+        return datos;
+    }
+
+    public Productos buscaId(long idproducto){
+        return datos.stream()
+        .filter(p->p.getIdProducto()
+        ==idproducto)
+        .findFirst().orElse(null);
+    }
+
+}
