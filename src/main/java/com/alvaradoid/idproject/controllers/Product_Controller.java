15564d6ea7526bea38aabe6858a5f8dd43a3d286
Alvarado007 <samuel.alvarado@correounivalle.edu.co>
2025-03-13 08:25:13 -0500

    se sigue trabajando en inyeccion de dependencias y como retornar mi lista de valores desde un reques en el url

---
+++ src/main/java/com/alvaradoid/idproject/controllers/Product_Controller.java
@@ -0,0 +1,27 @@
+package com.alvaradoid.idproject.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alvaradoid.idproject.models.Productos;
+import com.alvaradoid.idproject.services.Productos_Services;
+
+@RestController
+@RequestMapping("/api")
+public class Product_Controller {
+    private Productos_Services servicios = new Productos_Services();
+    @GetMapping()
+    public List<Productos> ListProductos(){
+        return servicios.finAll();
+    }
+
+    @GetMapping("/{idProducto}")
+    public Productos mostrar(@PathVariable long idProducto){
+        return servicios.buscaId(idProducto);
+    }
+
+}
